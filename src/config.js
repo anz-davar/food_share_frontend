a869362f8e99039d604b3d9a1cc329c59044c29a
anz-davar <anzori94@gmail.com>
2025-01-20 02:22:50 +0200

    fix typo

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: 'https://food-share-backend.onrender.com/', // Note the trailing slash!
+  API_URL: 'https://food-share-backend.onrender.com', // Note the trailing slash!
 };
 
 export default config;
